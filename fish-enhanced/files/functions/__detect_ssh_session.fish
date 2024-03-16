1b62c9982ecdd1bbe41152567441a4e3fee940d3
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2024-03-16 20:04:59 +0100

    Update session detection logic in __detect_ssh_session.fish and rebuild fish-enhanced package

--- fish-enhanced/files/functions/__detect_ssh_session.fish
+++ fish-enhanced/files/functions/__detect_ssh_session.fish
@@ -5,13 +5,23 @@ function __detect_ssh_session
     if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
         set -g SESSION_TYPE remote/ssh
     else
-        set -l fish_ppid (ps -o ppid= -p $fish_pid | string trim)
-        switch (ps -o comm= -p $fish_ppid)
-            case sshd
-            case '*/sshd'
-                set -g SESSION_TYPE remote/ssh
-            case '*'
-                set -g SESSION_TYPE local
+        # Traverse the process tree to determine the session type
+        # We might be using ssh but, we lost environment variables
+        # This might happen when using sudo/su
+        set -l fish_ppid $fish_pid
+        while test $fish_ppid -gt 1
+            set fish_ppid (ps -o ppid= -p $fish_ppid | string trim)
+            echo (ps -o comm= -p $fish_ppid)
+            switch (ps -o comm= -p $fish_ppid)
+                case sshd
+                case '*/sshd'
+                    set -g SESSION_TYPE remote/ssh
+                    return 0
+                case 'systemd'
+                    set -g SESSION_TYPE local
+                    return 0
+            end
         end
+        set -g SESSION_TYPE local
     end
 end
--- fish-enhanced/fish-enhanced.spec
+++ fish-enhanced/fish-enhanced.spec
@@ -1,6 +1,6 @@
 Name:           fish-enhanced
 Version:        0.2
-Release:        6%{?dist}
+Release:        7%{?dist}
 BuildArch:      noarch
 Summary:        Szpadel's opinionated fish enhancements
 
@@ -33,6 +33,9 @@ cp -r conf.d/* %{buildroot}/etc/fish/conf.d/
 
 
 %changelog
+* Sat Mar 16 2024 Piotr Rogowski <piotr.rogowski@creativestyle.pl> - 0.2-7
+- rebuilt
+
 * Sat Mar 16 2024 Piotr Rogowski <piotr.rogowski@creativestyle.pl> - 0.2-6
 - rebuilt
 
